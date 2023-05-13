dc005d1dbe51c2d03dbf79c1d79b8746dfb5f4e5
ravindu30 <ravindu30@gmail.com>
2023-05-13 01:47:19 -0400

    Final changes2

--- machinetranslation/tests.py
+++ machinetranslation/tests.py
@@ -1,44 +1,18 @@
 import unittest
 from translator import english_to_french, french_to_english
 
+class TestE2F(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(english_to_french(None), None, 'None should return None')
+        self.assertEqual(english_to_french("Hello"), "Bonjour", "Hello should be translated to Bonjour")
+        self.assertNotEqual(english_to_french("Goodbye"), "Chaise", "Goodbye should not be translated to Chaise")
+        self.assertEqual(english_to_french("Goodbye"), "Au revoir", "Goodbye should be translated to Au revoir")
 
-class TestTranslateFunctions(unittest.TestCase):
-    def test_nul(self):
-        self.assertEqual(english_to_french(None), None)
-        self.assertEqual(french_to_english(None), None)
+class TestF2E(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(french_to_english(None), None, 'None should return None')
+        self.assertEqual(french_to_english("Bonjour"), "Hello", "Bonjour should be translated to Hello")
+        self.assertNotEqual(french_to_english("Au revoir"), "Chair", "Au revoir should not be translated to Chair")
+        self.assertEqual(french_to_english("Au revoir"), "Goodbye", "Au revoir should be translated to Goodbye")
 
-
-    def test_space(self):
-        self.assertEqual(english_to_french(' '), ' ')
-        self.assertEqual(french_to_english(' '), ' ')
-
-
-    def test_hello(self):
-        self.assertEqual(english_to_french('Hello'), 'Bonjour')
-        self.assertEqual(french_to_english('Bonjour'),'Hello' )
-
-
-    def test_new_line(self):
-        self.assertEqual(english_to_french(
-            "I'm tired today.\nHello"),
-            "Je suis fatigué aujourd'hui.\nBonjour"
-        )
-        self.assertEqual(french_to_english(
-            "Je suis fatigué aujourd'hui.\nBonjour"),
-            "I'm tired today.\nHello"
-        )
-
-    
-    def test_untranslatable(self):
-        self.assertEqual(english_to_french(
-            "Глупый тест"),
-            "Глупый тест"
-        )
-        self.assertEqual(french_to_english(
-            "Глупый тест"),
-            "Глупый тест"
-        )
-
-
-if __name__ == '__main__':
-    unittest.main()
+unittest.main()
--- machinetranslation/translator.py
+++ machinetranslation/translator.py
@@ -1,38 +1,39 @@
 import os
+from dotenv import load_dotenv
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-APIKEY="zNTkM7s5ZFjfHdmPbgz_aiVLBr_DnrNbIknD6IitwrZ8"
-URL="https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/2b274465-b98f-4d46-87db-957a0b01343f" 
-authenticator = IAMAuthenticator(APIKEY)
-language_translator = LanguageTranslatorV3(
-version='2018-05-01',
-authenticator=authenticator)
-language_translator.set_service_url(URL)
+load_dotenv()
+api_key="zNTkM7s5ZFjfHdmPbgz_aiVLBr_DnrNbIknD6IitwrZ8"
+url="https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/2b274465-b98f-4d46-87db-957a0b01343f" 
 
-def english_to_french(english_text: str):
-    """Function takes the string in englsh and return translated string in french"""
+
+authenticator = IAMAuthenticator(api_key)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator)
+
+language_translator.set_service_url(url)
+
+def english_to_french(english_text):
+
     try:
         translation = language_translator.translate(
             text=english_text,
             model_id='en-fr').get_result()
-        #Getting the pure translation string
         french_text = translation['translations'][0]['translation']
-        return french_text
-    except Exception as error:
-        print(error)
-        return None
+    except:
+        french_text = None
 
+    return french_text
 
-def french_to_english(french_text: str):
-    """Function takes the string in french and return translated string in english"""
+def french_to_english(french_text):
+
     try:
         translation = language_translator.translate(
             text=french_text,
             model_id='fr-en').get_result()
-        #Getting the pure translation string
         english_text = translation['translations'][0]['translation']
-        return english_text
-    except Exception as error:
-        print(error)
-        return None
+    except:
+        english_text = None
+    return english_text
